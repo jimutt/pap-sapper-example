2d9008eaf5edf2236449ab8874193f7518538a74
Jimmy Utterström <jimutt@outlook.com>
2019-10-24 20:15:07 +0200

    Demo project for svelte-pick-a-place issue #12

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+  import PickAPlace from "svelte-pick-a-place";
+
+  let leaflet;
+
+  onMount(async () => {
+    leaflet = (await import("leaflet")).default;
+  });
+</script>
+
+<style>
+  .pick-a-place-wrapper {
+    width: 100%;
+    height: 800px;
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<div class="pick-a-place-wrapper">
+  {#if leaflet != null}
+    <PickAPlace {leaflet} on:update={() => console.log('Update!')} />
+  {/if}
+</div>
